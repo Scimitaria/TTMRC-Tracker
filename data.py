ab33fe289b99fc9b9c5b451a9370da50bfe61f36
Morgan Powers <morgan.c.powers@gmail.com>
2026-02-20 21:27:26 -0600

    abstracted tables into getResults

--- data.py
+++ data.py
@@ -1,74 +1,99 @@
 import requests # type: ignore
+from io import StringIO
 from datetime import date
+import pandas as pd # type: ignore
 from bs4 import BeautifulSoup as bs # type: ignore
+
+# prevents pandas from cutting data
+pd.set_option('display.max_rows',None)
 
 cur_year = date.today().year
 days = date.today().timetuple().tm_yday
 
+def getResults(event,year,dist):
+    url = "http://edsresults.com/{}{}/index.php?search_type=race_results&event={}&gender=&results_per_page=1000/".format(event,year,dist)
+    page = requests.get(url).content
+    table = pd.read_html(StringIO(str(bs(page,features="lxml").find_all('table',{'id':'data'}))))[0]
+    finishers = table.loc[table['Status']=='Complete']
+    for index,table in list(finishers.iterrows()):
+        data=str(table).splitlines()[:-1]
+        name = (str(data[4].split(" ")[-1])+" "+str(data[5].split(" ")[-1])).lower()
+        m=0
+        if   "100M" in dist: m+=100
+        elif "100K" in dist: m+=62.1
+        elif "50M" in dist: m+=50
+        elif "50K" in dist: m+=31.1
+
+def getResultsRocky(year,event,dist):
+    url = "http://edsresults.com/{}rr{}/index.php?search_type=race_results&event={}&gender=&results_per_page=1000".format(year,event,dist)
+    page = requests.get(url).content
+    table = pd.read_html(StringIO(str(bs(page,features="lxml").find_all('table',{'id':'data'}))))[0]
+    finishers = table.loc[table['Status']=='Complete']
+    for index,table in list(finishers.iterrows()):
+        data=str(table).splitlines()[:-1]
+        print(str(data[4].split(" ")[-1]),str(data[5].split(" ")[-1]))
+
 #Get Bandera results
 b_y = str(cur_year if days > 20 else cur_year-1)[-2:]
-#bandera100 = requests.get("http://edsresults.com/bandera{}/index.php?search_type=race_results&event=100K&gender=&results_per_page=400/".format(b_y)
-#bandera50  = requests.get("http://edsresults.com/bandera{}/index.php?search_type=race_results&event=50K&gender=&results_per_page=400/".format(b_y)
-#bandera50S = requests.get("http://edsresults.com/bandera{}/index.php?search_type=race_results&event=Saturday+50K&gender=&results_per_page=400/".format(b_y)
+#getResults("bandera",b_y,"100K")
+#getResults("bandera",b_y,"50K")
+#getResults("bandera",b_y,"Saturday+50K")
 
 #Get Rocky Raccoon results
 rr_y = cur_year if days > 50 else cur_year-1
-#rocky100M = requests.get("http://edsresults.com/{}rr100/index.php?search_type=race_results&event=100M&gender=&results_per_page=400".format(rr_y))
-#rocky100K = requests.get("http://edsresults.com/{}rr100/index.php?search_type=race_results&event=100K&gender=&results_per_page=400".format(rr_y))
-#rocky50M  = requests.get("http://edsresults.com/{}rr50/index.php?search_type=race_results&event=50M&gender=&results_per_page=400".format(rr_y))
-#rocky50K  = requests.get("http://edsresults.com/{}rr50/index.php?search_type=race_results&event=50K&gender=&results_per_page=400".format(rr_y))
+#getResultsRocky(rr_y,100,'100M')
+#getResultsRocky(rr_y,100,'100K')
+#getResultsRocky(rr_y,50,'50M')
+#getResultsRocky(rr_y,50,'50K')
 
 #Get Great Springs Austin results
 gsa_y = str(cur_year if days > 75 else cur_year-1)[-2:]
-#gsa50k = requests.get("http://edsresults.com/austin{}/index.php?search_type=race_results&event=50K&gender=&results_per_page=400".format(gs_y))
-#gsaMar = requests.get("http://edsresults.com/austin{}/index.php?search_type=race_results&event=26.2M&gender=&results_per_page=400".format(gs_y))
-
-#Get Hippo results
-h_y = cur_year if days > 90 else cur_year-1
-#hippoMar = requests.get("http://edsresults.com/hippo{}/index.php?search_type=race_results&event=26.2M&gender=&results_per_page=400".format(str(h_y)[-2:]))
+#getResults("austin",gsa_y,'50K')
 
 #Get Hells Hills results
 hh_y = str(cur_year if days > 100 else cur_year-1)[-2:]
-#hh50M = requests.get("http://edsresults.com/hh{}/index.php?search_type=race_results&event=50M&gender=&results_per_page=400".format(hh_y))
-#hh50K = requests.get("http://edsresults.com/hh{}/index.php?search_type=race_results&event=50K&gender=&results_per_page=400".format(hh_y))
+#getResults("hh",hh_y,'50M')
+#getResults("hh",hh_y,'50K')
 
 #Get Pandora results
 pb_y = str(cur_year if days > 120 else cur_year-1)[-2:]
-#pb_DMar = requests.get("http://edsresults.com/rox{}/index.php?search_type=race_results&event=52.4M&gender=&results_per_page=400".format(hh_y))
-#pb_Mar  = requests.get("http://edsresults.com/rox{}/index.php?search_type=race_results&event=26.2M&gender=&results_per_page=400".format(hh_y))
+#getResults("rox",pb_y,'52.4M')
 
 #Get Dirt Fest results
 df_y = str(cur_year if days > 135 else cur_year-1)[-2:]
-#df_50K = requests.get("http://www.edsresults.com/dirtfest{}/index.php?search_type=race_results&event=50K&gender=&results_per_page=400".format(df_y))
+#getResults("dirtfest",df_y,'50K')
 
 #Get River's Edge results
 re_y = str(cur_year if days > 150 else cur_year-1)[-2:]
-#re50K = requests.get("http://edsresults.com/edge{}/index.php?search_type=race_results&event=50K&gender=&results_per_page=400".format(re_y))
+#getResults("edge",re_y,'50K')
 
 #Get Great Springs Canyon Lake results
 gscl_y = str(cur_year if days > 160 else cur_year-1)[-2:]
-#gscl50K = requests.get("http://edsresults.com/canyonlake{}/index.php?search_type=race_results&event=50K&gender=&results_per_page=400".format(gscl_y))
-
-#Get Mellow results
-m_y = str(cur_year if days > 260 else cur_year-1)[-2:]
-#mellowMar = requests.get("http://edsresults.com/mellow{}/index.php?search_type=race_results&event=26.2M&gender=&results_per_page=400".format(m_y))
+#getResults("canyonlake",gscl_y,'50K')
 
 #Get Trailway results
 tt_y = str(cur_year if days > 290 else cur_year-1)[-2:]
-#tt50K = requests.get("http://edsresults.com/trailway{}/index.php?search_type=race_results&event=50K&gender=&results_per_page=400".format(tt_y))
-#ttMar = requests.get("http://edsresults.com/trailway{}/index.php?search_type=race_results&event=26.2M&gender=&results_per_page=400".format(tt_y))
+#getResults("trailway",tt_y,'50K')
+
 
 #Get Cactus Rose results
 cr_y = str(cur_year if days > 305 else cur_year-1)[-2:]
-#cr100M = requests.get("http://edsresults.com/cr{}/index.php?search_type=race_results&event=100M&gender=&results_per_page=400".format(cr_y))
-#cr50M  = requests.get("http://edsresults.com/cr{}/index.php?search_type=race_results&event=50M&gender=&results_per_page=400".format(cr_y))
+#getResults("cr",cr_y,'100M')
+#getResults("cr",cr_y,'50M')
 
 #Get Wild Hare results
 wh_y = str(cur_year if days > 325 else cur_year-1)[-2:]
-#wh50M = requests.get("http://edsresults.com/wildhare{}/index.php?search_type=race_results&event=50M&gender=&results_per_page=400".format(cr_y))
-#wh50K = requests.get("http://edsresults.com/wildhare{}/index.php?search_type=race_results&event=50K&gender=&results_per_page=400".format(cr_y))
+#getResults("wildhare",wh_y,'50M')
+#getResults("wildhare",wh_y,'50K')
 
 #Get Mosaic results
 m_y = str(cur_year if days > 345 else cur_year-1)[-2:]
-#mosaic50K = requests.get("http://edsresults.com/mosaic{}/index.php?search_type=race_results&event=50K&gender=&results_per_page=400".format(m_y))
-#mosaicMar = requests.get("http://edsresults.com/mosaic{}/index.php?search_type=race_results&event=26.2M&gender=&results_per_page=400".format(m_y))
+#getResults("mosaic",m_y,'50K')
+
+
+# Deprecated due to being short #
+#Get Hippo results
+#h_y = str(cur_year if days > 90 else cur_year-1)[-2:]
+#Get Mellow results
+#m_y = str(cur_year if days > 260 else cur_year-1)[-2:]
+#mellowMar = requests.get("http://edsresults.com/mellow{}/index.php?search_type=race_results&event=26.2M&gender=&results_per_page=400".format(m_y))
